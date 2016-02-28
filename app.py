6766239baa552973166f3e5de6cea7be4557d075
chitrankdixit <chitrank.dixit@flyrobe.com>
2016-02-29 01:14:50 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,12 @@
+#!flask/bin/python
+
+"""Alternative version of the ToDo RESTful server implemented using the
+Flask-RESTful extension."""
+
+from flask import Flask, jsonify, abort, make_response
+from flask.ext.restful import Api, Resource, reqparse, fields, marshal
+from flask.ext.httpauth import HTTPBasicAuth
+
+app = Flask(__name__, static_url_path="")
+api = Api(app)
+auth = HTTPBasicAuth()
