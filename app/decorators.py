4b630fd423c4ae9d95f95be697d64a0c1624dcdd
Chitrank-Dixit <chitrankdixit@gmail.com>
2017-02-06 13:42:44 +0530

    update token decorator

--- app/decorators.py
+++ app/decorators.py
@@ -4,78 +4,14 @@
 
 import datetime
 from app import User, oauth, db
-from app.models import Token, Grant, Client
+from app.models import Grant
+from functools import wraps
 
-@oauth.clientgetter
-def load_client(client_id):
-    return Client.query.filter_by(client_id=client_id).first()
-
-
-
-@oauth.grantgetter
-def load_grant(client_id, code):
-    return Grant.query.filter_by(client_id=client_id, code=code).first()
-
-@oauth.grantsetter
-def save_grant(client_id, code, request, *args, **kwargs):
-    # decide the expires time yourself
-    expires = datetime.datetime.utcnow() + datetime.timedelta(seconds=100)
-    grant = Grant(
-        client_id=client_id,
-        code=code['code'],
-        redirect_uri=request.redirect_uri,
-        _scopes=' '.join(request.scopes),
-        user=request.user, # at place of get_current_user()
-        expires=expires
-    )
-    db.session.add(grant)
-    db.session.commit()
-    return grant
-
-@oauth.tokengetter
-def load_token(access_token=None, refresh_token=None):
-    if access_token:
-        return Token.query.filter_by(access_token=access_token).first()
-    elif refresh_token:
-        return Token.query.filter_by(refresh_token=refresh_token).first()
+def set_cookie(f):
+    @wraps(f)
+    def decorated_function(*args, **kws):
+        # TODO: Check for correct token here
+        return f(*args, **kws)
+    return decorated_function
 
 
-@oauth.tokensetter
-def save_token(token, request, *args, **kwargs):
-    toks = Token.query.filter_by(client_id=request.client.client_id,
-                                 user_id=request.user.id)
-    # make sure that every client has only one token connected to a user
-    for t in toks:
-        db.session.delete(t)
-
-    expires_in = token.get('expires_in')
-    expires = datetime.datetime.utcnow() + datetime.timedelta(seconds=expires_in)
-
-    tok = Token(
-        access_token=token['access_token'],
-        refresh_token=token['refresh_token'],
-        token_type=token['token_type'],
-        _scopes=token['scope'],
-        expires=expires,
-        client_id=request.client.client_id,
-        user_id=request.user.id,
-    )
-    db.session.add(tok)
-    db.session.commit()
-    return tok
-
-@oauth.usergetter
-def get_user(username, password, *args, **kwargs):
-    user = User.query.filter_by(username=username).first()
-    if user.check_password(password):
-        return user
-    return None
-
-
-# @app.oauthv2_authentication
-# def detect_something():
-#     header = request.headers.get('Authorization')
-#     if header:
-#         _, token = header.split()
-#         request.identity = identity(jwt.decode(token,
-#                                                app.config['SECRET_KEY']))
